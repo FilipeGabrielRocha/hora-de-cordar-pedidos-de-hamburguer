26de493440abd8c41f48ceb8a585babe8ef191e0
Filipe Gabriel Rocha <filiperochaprogramador@gmail.com>
2022-10-05 10:54:44 -0300

    Aula 10 - curso de vue
    Aula 10 = Argumentos

--- 2_projeto_vue/src/components/Info.vue
+++ 2_projeto_vue/src/components/Info.vue
@@ -9,17 +9,24 @@
             <li>CSS 3</li>
         </ul>
         <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
+        <p>Para acessar meu portfólio <a v-bind:href="meu_link" target="_blank">basta clicar aqui</a></p>
+        <Picture />
     </div>
 </template>
 <script>
+import Picture from './Picture.vue';
     export default {
-        name: 'Info',
-        data() {
-            return {
-                esta_trabalhando: true,
-                mostrar_email: true,
-                email: 'filiperochaprogramador@gmail.com'
-            }
-        }
+    name: "Info",
+    data() {
+        return {
+            esta_trabalhando: true,
+            mostrar_email: true,
+            email: "filiperochaprogramador@gmail.com",
+            meu_link: "https://filipegabrielrocha.github.io/portfolio/"
+        };
+    },
+    components: { 
+        Picture
     }
+}
 </script>
---
+++ 2_projeto_vue/src/components/Picture.vue
@@ -0,0 +1,19 @@
+<template>
+    <img :src="avatar" v-bind:alt="descricao">
+</template>
+<script>
+    export default {
+        name: 'Picture',
+        data() {
+            return {
+                avatar: '/img/horizon_forbidden_west.jpg',
+                descricao: 'Foto de um jogo'
+            }
+        }
+    }
+</script>
+<style>
+    img {
+      width: 450px;
+    }
+</style>
