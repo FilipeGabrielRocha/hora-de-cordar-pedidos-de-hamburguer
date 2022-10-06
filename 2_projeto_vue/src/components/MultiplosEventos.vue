c4da15a8a809a2047fbc14c311464294355cdbee
Filipe Gabriel Rocha <filiperochaprogramador@gmail.com>
2022-10-06 10:54:52 -0300

    Aula 15 - curso de vue
    Múltiplos Eventos

---
+++ 2_projeto_vue/src/components/MultiplosEventos.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <button @click="primeiro($event), segundo($event)">Ativar múltiplos eventos</button>
+        <p>{{ multiplos_eventos }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'MultiplosEventos',
+        data() {
+            return {
+                multiplos_eventos: ''
+            }
+        },
+        methods: {
+            primeiro(e) {
+                this.multiplos_eventos = "Primeiro evento ativado!"
+            },
+            segundo(e) {
+                setTimeout(() => this.multiplos_eventos = "Segundo evento!", 500)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        margin: 100px;
+    }
+</style>
--- 2_projeto_vue/src/components/PrimeiroComponente.vue
+++ 2_projeto_vue/src/components/PrimeiroComponente.vue
@@ -2,17 +2,25 @@
     <div>
         <h1>Olá Vue!</h1>
         <p>Meu nome é {{ nome }} e trabalho com {{profissao}}</p>
+        <MultiplosEventos />
     </div>
 </template>
 
 <script>
+    import MultiplosEventos from './MultiplosEventos.vue';
     export default {
         name: 'PrimeiroComponente',
+        components: {
+            MultiplosEventos
+        },
         data() {
             return {
                 nome: "Filipe",
                 profissao: "Programador"
             }
+        },
+        methods: {
+
         }
     }
 </script>
