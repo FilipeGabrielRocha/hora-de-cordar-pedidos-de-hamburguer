7335881daf2b1ca515f8ef9d53eb51aa9f8999c6
Filipe Gabriel Rocha <filiperochaprogramador@gmail.com>
2022-10-06 10:42:31 -0300

    Aula 14 - curso de vue
    Aula 14 - eventos

--- 2_projeto_vue/src/components/Form.vue
+++ 2_projeto_vue/src/components/Form.vue
@@ -1,16 +1,21 @@
 <template>
-    <form action="">
-        <h1>Meu formulário:</h1>
-        <div>
-            <InputText />
-        </div>
-        <div>
-            <InputText />
-        </div>
-        <div>
-            <Submit />
-        </div>
-    </form>
+    <div>
+        <form action="" @submit="enviarFormulario($event)">
+            <h1>Meu formulário:</h1>
+            <div>
+                <input type="text" v-model="name" name="" id="">
+            </div>
+            <div>
+                <input type="text" v-model="email" name="" id="">
+            </div>
+            <div>
+                <Submit />
+            </div>
+        </form>
+        <p>Conteúdo</p>
+        <p>Conteúdo</p>
+        <p>Conteúdo</p>
+    </div>
 </template>
 <script>
 import InputText from './form/InputText.vue';
@@ -18,9 +23,39 @@ import Submit from './form/Submit.vue';
     export default {
     name: "Form",
     components: {
-    InputText,
-    Submit,
-    InputText
-}
+        InputText,
+        Submit,
+        InputText
+    },
+    data() {
+        return {
+            name: '',
+            email: ''
+        }
+    },
+    methods: {
+        enviarFormulario(e) {
+
+            e.preventDefault();
+
+            const name = this.name;
+            const email = this.email;
+
+            console.log('Formulario enviado')
+            console.log(`O nome é ${name}`)
+            console.log(`O e-mail é ${email}`)
+
+            // ajax
+
+
+            //inserir no banco os dados
+        },
+    },
 }
 </script>
+
+<style scoped>
+    p {
+        margin: 150px;
+    }
+</style>
