8fe0e227d92cb3fc83e0416e0ff773f161aae329
Filipe Gabriel Rocha <filiperochaprogramador@gmail.com>
2022-10-07 10:09:34 -0300

    Aula 18 - curso de vue
    Aula 18 - Ouvindo eventos com o $emit

--- 2_projeto_vue/src/components/Picture.vue
+++ 2_projeto_vue/src/components/Picture.vue
@@ -1,16 +1,28 @@
 <template>
-    <img :src="avatar" v-bind:alt="descricao">
+    <div>
+        <img :src="avatar" v-bind:alt="descricao">
+        <MudarImagem @mudar-imagem="trocarImagem" />
+    </div>
 </template>
 <script>
+import MudarImagem from './MudarImagem.vue';
     export default {
-        name: 'Picture',
-        data() {
-            return {
-                avatar: '/img/horizon_forbidden_west.jpg',
-                descricao: 'Foto de um jogo'
-            }
+    name: "Picture",
+    data() {
+        return {
+            avatar: "/img/horizon_forbidden_west.jpg",
+            descricao: "Foto de um jogo"
+        };
+    },
+    components: { 
+        MudarImagem 
+    },
+    methods: {
+        trocarImagem() {
+            this.avatar = "/img/thelast.jpg"
         }
     }
+}
 </script>
 <style>
     img {
